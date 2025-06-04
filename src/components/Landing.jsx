aae8a930ed6e8f1bb8f46b85c5ef4884b6fb610f
Samuel <srangel674@gmail.com>
2025-06-04 09:49:55 -0700

    changed possible typo

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,22 +1,22 @@
-import QuickSearch from "./QuickSeach";
+import QuickSearch from "./QuickSearch";
 
 function Landing() {
   return (
-      <div id="landing">
-        <div className="hero">
-  <h1 className="hero__title">Welcome to the D<span className="red">&</span>D Helper</h1>
-  <p className="hero__subtitle">
-    Welcome to DnD Helper! This website is designed to assist players 
-    and Dungeon Masters in their Dungeons & Dragons adventures. 
-    Here, you can explore spells, monsters, and other resources to 
-    enhance your gameplay experience.
-  </p>
-  <p className="hero__subtitle">
-    Search for a spell or monster to get started.
-  </p>
-  <QuickSearch />
-</div>
+    <div id="landing">
+      <div className="hero">
+        <h1 className="hero__title">Welcome to the D<span className="red">&</span>D Helper</h1>
+        <p className="hero__subtitle">
+          Welcome to DnD Helper! This website is designed to assist players
+          and Dungeon Masters in their Dungeons & Dragons adventures.
+          Here, you can explore spells, monsters, and other resources to
+          enhance your gameplay experience.
+        </p>
+        <p className="hero__subtitle">
+          Search for a spell or monster to get started.
+        </p>
+        <QuickSearch />
       </div>
+    </div>
   );
 }
 
