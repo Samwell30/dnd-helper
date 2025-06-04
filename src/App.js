36fc91113184f8ea583244fa5b0da60b906cb370
Samuel <srangel674@gmail.com>
2025-06-04 10:26:09 -0700

    added original app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,38 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./components/Landing";
+import SpellsList from "./pages/Spells";
+import Monsters from "./pages/Monsters";
+import Nav from "./components/Nav";
+import SpellDetails from "./pages/SpellDetails";
+import MonsterDetail from "./pages/MonsterDetail";
+import Footer from "./components/Footer";
+import Equipment from "./pages/Equipment";
+import MagicItemList from "./pages/MagicItems";
+import Classes from "./pages/Classes";
+import ClassDetail from "./pages/ClassDetail";
+import Features from "./pages/Features";
+
+
 function App() {
-  return <div style={{ color: "black", fontSize: 32 }}>Hello, world!</div>;
+  return (
+    <BrowserRouter>
+      <div id="app">
+        <Nav />
+        <Routes>
+          <Route path="/spells" element={<SpellsList />} />
+          <Route path="/spells/:spellIndex" element={<SpellDetails />} />
+          <Route path="/monsters" element={<Monsters />} />
+          <Route path="/monsters/:monsterIndex" element={<MonsterDetail />} />
+          <Route path="/equipment" element={<Equipment />} />
+          <Route path="/magicItems" element={<MagicItemList />} />
+          <Route path="/classes" element={<Classes />} />
+          <Route path="/classes/:classIndex" element={<ClassDetail />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="*" element={<Landing />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
 export default App;
