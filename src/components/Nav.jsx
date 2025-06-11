9e689c21ed98ae7db8a5a8f4c43399c24ed777f8
Samuel <srangel674@gmail.com>
2025-06-11 09:28:21 -0700

    small changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,15 +4,16 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <nav>
-        <ul className="nav__list">
-            <li className="nav__item"><Link to="/">Home</Link></li>
-            <li className="nav__item"><Link to="/spells">Spells</Link></li>
-            <li className="nav__item"><Link to="/monsters">Monsters</Link></li>
-            <li className="nav__item"><Link to="/equipment">Equipment</Link></li>
-            <li className="nav__item"><Link to="/magicItems">Magic Items</Link></li>
-            <li className="nav__item"><Link to="/classes">Classes</Link></li>
-            <li className="nav__item"><Link to="/rules">Rules</Link></li>
-        </ul>
+      <ul className="nav__list">
+        <li className="nav__item"><Link to="/">Home</Link></li>
+        <li className="nav__item"><Link to="/spells">Spells</Link></li>
+        <li className="nav__item"><Link to="/monsters">Monsters</Link></li>
+        <li className="nav__item"><Link to="/equipment">Equipment</Link></li>
+        <li className="nav__item"><Link to="/magicItems">Magic Items</Link></li>
+        <li className="nav__item"><Link to="/classes">Classes</Link></li>
+        <li className="nav__item"><Link to="/rules">Rules</Link></li>
+        <li className="nav__item"><Link to="/traits">Traits</Link></li>
+      </ul>
     </nav>
   );
 };
